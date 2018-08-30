bc67b116adf9ae3f471a41120519573d4cc931f5
Muralidhar Gumma <muralidhargumma007@gmail.com>
2018-08-31 00:10:32 +0530

    Committing One more time

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are
+                    You are logged! in
                 </div>
             </div>
         </div>
